181b7cdc02e880dec50fda75f61b055b5577151a
Tiago Villa <tiago_villa1@hotmail.com>
2022-03-04 07:21:56 -0300

    uuid domain

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,6 +1,7 @@
 import {Category, CategoryProperties} from "./category";
 import {omit} from 'lodash';
 import {validate as uuidValidate} from 'uuid';
+import UniqueEntityId from "../../../@seedwork/domain/unique-entity-id.vo";
 
 describe('Category Unit Tests', () => {
   test('constructor of category', () => {
@@ -52,18 +53,18 @@ describe('Category Unit Tests', () => {
   })
 
   test('id prop', () => {
-    type CategoryData = { props: CategoryProperties, id?: string }
+    type CategoryData = { props: CategoryProperties, id?: UniqueEntityId }
     const data: CategoryData[] = [
       {props: {name: 'Movie'}},
       {props: {name: 'Movie'}, id: null},
       {props: {name: 'Movie'}, id: undefined},
-      {props: {name: 'Movie'}, id: 'be145867-3880-4e36-80a1-e8b967b6a35d'},
+      {props: {name: 'Movie'}, id: new UniqueEntityId()},
     ];
 
     data.forEach(i => {
-      const category = new Category(i.props, i.id);
+      const category = new Category(i.props, i.id as any);
       expect(category.id).not.toBeNull();
-      expect(uuidValidate(category.id)).toBeTruthy();
+      expect(category.id).toBeInstanceOf(UniqueEntityId);
     })
   })
 
